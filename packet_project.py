9c594785108de4bc5e80f66e598401517490b508
Niels Grewe <niels.grewe@halbordnung.de>
2016-06-18 01:48:53 +0200

    Duplicate lines

--- packet_project.py
+++ packet_project.py
@@ -23,8 +23,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-# Begin common code
-# It currently doesn't seem to be easily possible to
 DOCUMENTATION = '''
 ---
 module: packet_project
